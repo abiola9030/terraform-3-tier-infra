35a643a2e4ce67d05a29e5e8827d4007286229fc
Abiola Kadiri <abiolakadiri9030@gmail.com>
2023-11-16 01:28:54 +0000

    added ec2 module

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,3 +1,3 @@
-output "instance_ids" {
-    value = aws_instance.business_logic[*].id
-}
+# output "instance_ids" {
+#     value = aws_instance.business_logic[*].id
+# }
--- main.tf
+++ main.tf
@@ -12,11 +12,18 @@ module "rds" {
     vpc_cidr = "10.0.0.0/16"
 }
 
-module "ec2" {
-    source = "./ec2"
-    private_subnet_ids = module.vpc.private_subnets_ids
-    tags = local.project_tags
+module "ec2_" {
+    source = "./ec2(2)"
+    subnet_id = module.vpc.public_subnet1_id
     vpc_id = module.vpc.vpc_id
-    vpc_cidr_block = ["10.0.0.0/16"]
-
+    tags = local.project_tags
 }
+
+# module "ec2" {
+#     source = "./ec2"
+#     private_subnet_ids = module.vpc.private_subnets_ids
+#     tags = local.project_tags
+#     vpc_id = module.vpc.vpc_id
+#     vpc_cidr_block = ["10.0.0.0/16"]
+
+# }
